d05d51d7d9c617d0f67ee3aecdda63ffb012aa18
Mandara M <73648431+MANDARA-M@users.noreply.github.com>
2022-10-20 20:03:35 +0530

    Create python for datascience.py
    adding a new file

---
+++ python for datascience.py
@@ -0,0 +1,4 @@
+#display a print statement
+
+print("welcome to python for data science")
+print("we use python basics , and some packages namely \n matplotlib , \n numpy , \n pandas ,\n seaborn etc...")
